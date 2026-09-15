bb77b6ee16e6549c636080fc16b2222358eab93c
Bhawik <tushar.sharma17101@gmail.com>
2026-09-15 21:13:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
           },
         ],
         screenshots: [
-          {src: './mobile-landscape-view(1600x641).jpeg', sizes: '1600x641', type: 'image/jpg', form_factor: 'wide', label: 'Mobile Landscape View'},
+          {src: './mobile-landscape-view(1600x641).jpeg', sizes: '1600x641', type: 'image/jpeg', form_factor: 'wide', label: 'Mobile Landscape View'},
           {src: './mobile-portrait-view(726x1456).jpeg', sizes: '726x1456', type: 'image/jpeg', form_factor: 'narrow', label: 'Mobile Portrait View'},
           {src: './mobile-portrait-view(726x1467).jpeg', sizes: '726x1467', type: 'image/jpeg', form_factor: 'narrow', label: 'Mobile Portrait View Dark'},
           {src: './dark-mode-2(726x1492).jpeg', sizes: '726x1492', type: 'image/jpeg', form_factor: 'narrow', label: 'Mobile View'},
